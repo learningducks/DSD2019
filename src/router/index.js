686ed8dd5f9bb1f43c9950dfbe747305d55db00f
zxnm19980609 <zxnm19980609@outlook.com>
2019-04-21 11:27:06 +0800

    Add 404 Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 import Login from '@/views/Login';
+import NotFound from '@/views/404';
 
 Vue.use(Router);
 
@@ -16,6 +17,16 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: NotFound,
+    },
+    {
+      path: '*',
+      name: '404',
+      redirect: { path: '/404' },
     },
   ],
 });
