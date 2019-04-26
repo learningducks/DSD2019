016ebe99db6cbdaa832f95b771cff29214ec5441
zxnm19980609 <zxnm19980609@outlook.com>
2019-04-26 19:03:38 +0800

    finish navmenu

---
+++ src/views/Home.vue
@@ -0,0 +1,249 @@
+<template>
+  <el-row class="container">
+    <el-col :span="24" class="header">
+      <el-col
+        span="10"
+        class="logo"
+        :class="collapsed ? 'logo-collapse-width' : 'logo-width'"
+      >
+        {{ collapsed ? "" : sysName }}
+      </el-col>
+      <el-col :span="10">
+        <div class="tools" @click.prevent="collapse">
+          <i class="fa fa-align-justify"></i>
+        </div>
+      </el-col>
+      <el-col :span="4" class="userinfo">
+        <el-dropdown trigger="hover">
+          <span class="el-dropdown-link userinfo-inner">
+            <img src="@/assets/defaultAvatar.jpg" /> {{ nickname }}
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item disabled> {{ role }} </el-dropdown-item>
+            <el-dropdown-item divided @click.native="logout"
+              >登出</el-dropdown-item
+            >
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-col>
+    </el-col>
+    <el-col :span="24" class="main">
+      <aside :class="collapsed ? 'menu-collapsed' : 'menu-expanded'">
+        <el-menu
+          :default-active="$route.path"
+          class="el-menu-vertical-demo"
+          unique-opened
+          router
+          v-show="!collapsed"
+        >
+          <template
+            v-for="(item, index) in $router.options.routes"
+            v-if="!item.hidden"
+          >
+            <el-submenu :index="index" :key="index">
+              <template slot="title">
+                <i :class="item.icon"></i>{{ item.name }}
+              </template>
+              <el-menu-item
+                v-for="child in item.children"
+                :index="child.path"
+                :key="child.path"
+                v-if="!child.hidden"
+              >
+                {{ child.name }}
+              </el-menu-item>
+            </el-submenu>
+          </template>
+        </el-menu>
+        <ul
+          class="el-menu el-menu-vertical-demo collapsed"
+          v-show="collapsed"
+          ref="menuCollapsed"
+        ></ul>
+      </aside>
+      <section class="content-container">
+        <div class="grid-content bg-purple-light">
+          <el-col :span="24" class="breadcrumb-container">
+            <strong class="title">{{ $route.name }}</strong>
+            <el-breadcrumb separator="/" class="breadcrumb-inner">
+              <el-breadcrumb-item
+                v-for="item in $route.matched"
+                :key="item.path"
+              >
+                {{ item.name }}
+              </el-breadcrumb-item>
+            </el-breadcrumb>
+          </el-col>
+          <el-col :span="24" class="content-wrapper">
+            <transition name="fade" mode="out-in">
+              <router-view></router-view>
+            </transition>
+          </el-col>
+        </div>
+      </section>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sysName: '智能光控系统',
+      nickname: '阿伟',
+      role: '管理员',
+      collapsed: false,
+    };
+  },
+  methods: {
+    logout() {
+      this.$confirm('确认退出吗?', '提示', {}).then(() => {
+        sessionStorage.removeItem('user');
+        this.$router.push('Login');
+      }).catch(() => {
+
+      });
+    },
+    collapse() {
+      this.collapsed = !this.collapsed;
+    },
+  },
+  mounted() {
+    let user = sessionStorage.getItem('user');
+    if (user) {
+      user = JSON.parse(user);
+      this.nickname = user.nickname;
+    }
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/styles/color.scss";
+.container {
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  width: 100%;
+  .header {
+    height: 60px;
+    line-height: 60px;
+    background: $color-primary;
+    color: #fff;
+    .userinfo {
+      text-align: right;
+      padding-right: 35px;
+      float: right;
+      .userinfo-inner {
+        cursor: pointer;
+        color: #fff;
+        img {
+          width: 40px;
+          height: 40px;
+          border-radius: 20px;
+          margin: 10px 0px 10px 10px;
+          float: right;
+        }
+      }
+    }
+    .logo {
+      //width:230px;
+      height: 60px;
+      font-size: 22px;
+      padding-left: 20px;
+      padding-right: 20px;
+      border-color: rgba(238, 241, 146, 0.3);
+      border-right-width: 1px;
+      border-right-style: solid;
+      img {
+        width: 40px;
+        float: left;
+        margin: 10px 10px 10px 18px;
+      }
+      .txt {
+        color: #fff;
+      }
+    }
+    .logo-width {
+      width: 230px;
+    }
+    .logo-collapse-width {
+      width: 60px;
+    }
+    .tools {
+      padding: 0px 23px;
+      width: 14px;
+      height: 60px;
+      line-height: 60px;
+      cursor: pointer;
+    }
+  }
+  .main {
+    display: flex;
+    // background: #324057;
+    position: absolute;
+    top: 60px;
+    bottom: 0px;
+    overflow: hidden;
+    aside {
+      flex: 0 0 230px;
+      width: 230px;
+      // position: absolute;
+      // top: 0px;
+      // bottom: 0px;
+      .el-menu {
+        height: 100%;
+      }
+      .collapsed {
+        width: 60px;
+        .item {
+          position: relative;
+        }
+        .submenu {
+          position: absolute;
+          top: 0px;
+          left: 60px;
+          z-index: 99999;
+          height: auto;
+          display: none;
+        }
+      }
+    }
+    .menu-collapsed {
+      flex: 0 0 60px;
+      width: 60px;
+    }
+    .menu-expanded {
+      flex: 0 0 230px;
+      width: 230px;
+    }
+    .content-container {
+      // background: #f1f2f7;
+      flex: 1;
+      // position: absolute;
+      // right: 0px;
+      // top: 0px;
+      // bottom: 0px;
+      // left: 230px;
+      overflow-y: scroll;
+      padding: 20px;
+      .breadcrumb-container {
+        //margin-bottom: 15px;
+        .title {
+          width: 200px;
+          float: left;
+          color: #475669;
+        }
+        .breadcrumb-inner {
+          float: right;
+        }
+      }
+      .content-wrapper {
+        background-color: #fff;
+        box-sizing: border-box;
+      }
+    }
+  }
+}
+</style>
