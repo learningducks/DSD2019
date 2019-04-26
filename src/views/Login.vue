a5a59e740033385822d0121ec98d04625dfb1eba
zxnm19980609 <zxnm19980609@outlook.com>
2019-04-26 19:04:18 +0800

    finish login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,45 +1,47 @@
 <template>
-  <el-container>
-    <el-main>
-      <el-form
-        :model="loginForm"
-        :rules="rules"
-        ref="loginForm"
-        label-position="right"
-        label-width="0px"
-        class="login-container"
+  <el-form
+    :model="loginForm"
+    :rules="rules"
+    ref="loginForm"
+    label-position="right"
+    label-width="0px"
+    class="login-container"
+  >
+    <h3 class="title">系统登录</h3>
+    <el-form-item prop="username">
+      <el-input
+        v-model="loginForm.username"
+        type="text"
+        placeholder="用户名"
+      ></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input
+        v-model="loginForm.password"
+        type="password"
+        auto-complete="off"
+        placeholder="密码"
+      ></el-input>
+    </el-form-item>
+    <el-checkbox v-model="remember" disabled class="remember">
+      记住密码
+    </el-checkbox>
+    <el-form-item style="width: 100%;">
+      <el-button
+        type="primary"
+        plain
+        :loading="loading"
+        style="width: 100%;"
+        @click="lgoin"
+        >登录</el-button
       >
-        <h3 class="title">系统登录</h3>
-        <el-form-item prop="username">
-          <el-input
-            v-model="loginForm.username"
-            type="text"
-            placeholder="用户名"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            v-model="loginForm.password"
-            type="password"
-            auto-complete="off"
-            placeholder="密码"
-          ></el-input>
-        </el-form-item>
-        <el-checkbox v-model="remember" class="remember">记住密码</el-checkbox>
-        <el-form-item style="width: 100%;">
-          <el-button
-            type="primary"
-            style="width: 100%;"
-            @click.native.prevent="onSubmit"
-            >登录</el-button
-          >
-        </el-form-item>
-      </el-form>
-    </el-main>
-    <el-footer></el-footer>
-  </el-container>
+    </el-form-item>
+  </el-form>
 </template>
 <script>
+import { requestLogin } from '@/api/api';
+import md5 from 'js-md5';
+
 export default {
   data() {
     return {
@@ -56,7 +58,32 @@ export default {
         ],
       },
       remember: false,
+      loading: false,
     };
+  },
+  methods: {
+    login() {
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          this.loading = true;
+          const loginParams = { UID: this.loginForm.username, password: md5(`${this.loginForm.password}helloworld`) };
+          requestLogin(loginParams).then((data) => {
+            this.loading = false;
+            const { status, msg, userinfo } = data;
+            // eslint-disable-next-line eqeqeq
+            if (status != 0) {
+              this.$message({
+                message: msg,
+                type: 'error',
+              });
+            } else {
+              sessionStorage.setItem('userinfo', JSON.stringify(userinfo));
+              this.$router.push('Home');
+            }
+          });
+        }
+      });
+    },
   },
 };
 </script>
