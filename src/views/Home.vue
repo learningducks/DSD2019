a124054f467041d8f88806835d0f2853a4701a31
zxnm19980609 <zxnm19980609@outlook.com>
2019-06-09 17:31:58 +0800

    optimized UI & add Log

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
   <el-row class="container">
     <el-col :span="24" class="header">
-      <el-col
-        :span="10"
-        class="logo"
-        :class="collapsed ? 'logo-collapse-width' : 'logo-width'"
-      >
-        {{ collapsed ? "" : sysName }}
-      </el-col>
-      <el-col :span="10">
-        <div class="tools" @click.prevent="collapse">
-          <i class="fa fa-align-justify"></i>
-        </div>
+      <el-col :span="10" class="logo" style="width: 200px;">
+        <el-tooltip
+          content="Intelligent Light Control System"
+          placement="bottom"
+          effect="dark"
+        >
+          <span style="text-align: center; display: block;">{{ sysName }}</span>
+        </el-tooltip>
       </el-col>
       <el-col :span="4" class="userinfo">
         <el-dropdown trigger="hover">
@@ -21,80 +18,23 @@
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item disabled> {{ role }} </el-dropdown-item>
             <el-dropdown-item divided @click.native="logout"
-              >退出</el-dropdown-item
+              >Sign out</el-dropdown-item
             >
           </el-dropdown-menu>
         </el-dropdown>
       </el-col>
     </el-col>
     <el-col :span="24" class="main">
-      <aside :class="collapsed ? 'menu-collapsed' : 'menu-expanded'">
-        <el-menu
-          :default-active="$route.path"
-          unique-opened
-          router
-          v-show="!collapsed"
-        >
-          <template
-            v-for="(item, index) in $router.options.routes"
-            v-if="!item.hidden"
-          >
-            <el-submenu :index="index" :key="index">
-              <template slot="title">
-                <i :class="item.icon"></i>{{ item.name }}
-              </template>
-              <el-menu-item
-                v-for="child in item.children"
-                :index="child.path"
-                :key="child.path"
-                v-if="!child.hidden"
-              >
-                {{ child.name }}
+      <aside>
+        <el-menu :default-active="$route.path" router>
+          <template v-for="item in $router.options.routes">
+            <template v-for="route in item.children" v-if="!route.hidden">
+              <el-menu-item :index="route.path" :key="route.path">
+                <i :class="route.icon"></i>{{ route.name }}
               </el-menu-item>
-            </el-submenu>
+            </template>
           </template>
         </el-menu>
-        <ul
-          class="el-menu el-menu-vertical-demo collapsed"
-          v-show="collapsed"
-          ref="menuCollapsed"
-        >
-          <li
-            v-for="(item, index) in $router.options.routes"
-            :key="index"
-            v-if="!item.hidden"
-            class="el-submenu item"
-          >
-            <template>
-              <div
-                class="el-submenu__title"
-                style="padding-left: 20px;"
-                @mouseover="showMenu(index, true)"
-                @mouseout="showMenu(index, false)"
-              >
-                <i :class="item.icon"></i>
-              </div>
-              <ul
-                class="el-menu submenu"
-                :class="'submenu-hook-' + index"
-                @mouseover="showMenu(index, true)"
-                @mouseout="showMenu(index, false)"
-              >
-                <li
-                  v-for="child in item.children"
-                  v-if="!child.hidden"
-                  :key="child.path"
-                  class="el-menu-item"
-                  style="padding-left: 40px;"
-                  :class="$route.path == child.path ? 'is-active' : ''"
-                  @click="$router.push(child.path)"
-                >
-                  {{ child.name }}
-                </li>
-              </ul>
-            </template>
-          </li>
-        </ul>
       </aside>
       <section class="content-container">
         <div class="grid-content bg-purple-light">
@@ -109,9 +49,9 @@
               </el-breadcrumb-item>
             </el-breadcrumb>
           </el-col>
-          <el-col :span="24" class="content-wrapper">
+          <el-col :span="24" style="box-sizing: border-box;">
             <transition name="fade" mode="out-in">
-              <router-view></router-view>
+              <router-view />
             </transition>
           </el-col>
         </div>
@@ -121,41 +61,51 @@
 </template>
 
 <script>
+import { requestLogList } from '@/utils';
+
 export default {
   data() {
     return {
-      sysName: '智能光控系统',
+      sysName: 'ILCS',
       nickname: '',
       role: '',
-      collapsed: false,
+
+      logList: [],
     };
   },
   methods: {
     logout() {
       const This = this;
-      this.$confirm('确认退出吗?', '提示', {}).then(() => {
+      this.$confirm('Opt-out?', 'Confirm', {}).then(() => {
         This.$store.commit('clear');
         This.$router.push({ path: '/login' });
-        This.$message.success('退出成功');
+        This.$message.success('logout successfully');
       }).catch(() => {
 
       });
     },
-    collapse() {
-      // this.collapsed = !this.collapsed;
-    },
-    showMenu(i, status) {
-      this.$refs.menuCollapsed.getElementsByClassName(`submenu-hook-${i}`)[0].style.display = status ? 'block' : 'none';
+
+    getLogList() {
+      const params = { option: 0 };
+      requestLogList(params).then((data) => {
+        this.logList = data;
+        this.logList.forEach((log) => {
+          this.$notify.warning({
+            title: 'Warning',
+            message: 'TestLog',
+            duration: 0,
+          });
+        });
+      }).catch((err) => {
+        this.logList = [];
+        this.$message.error(err);
+      });
     },
   },
   mounted() {
     this.nickname = this.$store.getters.userinfo.nickname;
-    const roleName = {
-      admin: '管理员',
-      teacher: '教师',
-      student: '学生',
-    };
-    this.role = roleName[this.$store.getters.userinfo.role];
+    this.role = this.$store.getters.userinfo.role;
+    this.getLogList();
   },
 };
 </script>
@@ -179,7 +129,7 @@ export default {
       float: right;
       .userinfo-inner {
         cursor: pointer;
-        color: whitesmoke;
+        color: $color-text-primary;
         img {
           width: 40px;
           height: 40px;
@@ -191,7 +141,7 @@ export default {
     }
     .logo {
       height: 60px;
-      font-size: 22px;
+      font-size: 20px;
       padding-left: 20px;
       padding-right: 20px;
       border-color: rgba(238, 241, 146, 0.3);
@@ -205,12 +155,6 @@ export default {
       .txt {
         color: whitesmoke;
       }
-    }
-    .logo-width {
-      width: 230px;
-    }
-    .logo-collapse-width {
-      width: 60px;
     }
     .tools {
       padding: 0px 23px;
@@ -227,33 +171,11 @@ export default {
     bottom: 0px;
     overflow: hidden;
     aside {
-      flex: 0 0 230px;
-      width: 230px;
+      flex: 0 0 200px;
+      width: 200px;
       .el-menu {
         height: 100%;
       }
-      .collapsed {
-        width: 60px;
-        .item {
-          position: relative;
-        }
-        .submenu {
-          position: absolute;
-          top: 0px;
-          left: 60px;
-          z-index: 99999;
-          height: auto;
-          display: none;
-        }
-      }
-    }
-    .menu-collapsed {
-      flex: 0 0 60px;
-      width: 60px;
-    }
-    .menu-expanded {
-      flex: 0 0 230px;
-      width: 230px;
     }
     .content-container {
       flex: 1;
@@ -261,17 +183,14 @@ export default {
       padding: 20px;
       .breadcrumb-container {
         .title {
+          font-size: 20px;
           width: 200px;
           float: left;
-          color: #475669;
+          color: $color-primary;
         }
         .breadcrumb-inner {
           float: right;
         }
-      }
-      .content-wrapper {
-        background-color: whitesmoke;
-        box-sizing: border-box;
       }
     }
   }
