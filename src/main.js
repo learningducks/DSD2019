c80717048700fe13ccb4f17b1a0c8f3e813d62cc
zxnm19980609 <zxnm19980609@outlook.com>
2019-06-09 17:33:54 +0800

    remove mock & use element-ui with en

--- src/main.js
+++ src/main.js
@@ -2,18 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
 import 'element-ui/lib/theme-chalk/index.css';
 import 'font-awesome/css/font-awesome.min.css';
 import App from '@/App';
 import router from '@/router';
 import store from '@/store';
-import mock from '@/mock';
 import { generateRoutes } from '@/utils';
 
-mock.init();
-
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 router.beforeEach((to, from, next) => {
   if (to.path === '/') {
