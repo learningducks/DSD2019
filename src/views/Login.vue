02573c59c5c3c33a09fcf2e1340bc03a2bb92ec4
zxnm19980609 <zxnm19980609@outlook.com>
2019-06-09 17:28:33 +0800

    change lang to en-US & update login function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,16 +3,17 @@
     :model="loginForm"
     :rules="rules"
     ref="loginForm"
-    label-position="right"
     label-width="0px"
     class="login-container"
+    status-icon
   >
-    <h3 class="title">系统登录</h3>
+    <h3 class="title">Sign in</h3>
     <el-form-item prop="username">
       <el-input
         v-model.number="loginForm.username"
         type="text"
-        placeholder="用户名"
+        placeholder="username"
+        :auto-complete="on"
       ></el-input>
     </el-form-item>
     <el-form-item prop="password">
@@ -20,25 +21,27 @@
         v-model="loginForm.password"
         type="password"
         auto-complete="off"
-        placeholder="密码"
+        placeholder="password"
       ></el-input>
     </el-form-item>
     <el-checkbox v-model="remember" disabled class="remember">
-      记住密码
+      remember password
     </el-checkbox>
     <el-form-item style="width: 100%;">
       <el-button
         type="primary"
+        native-type="submit"
         plain
         :loading="loading"
         style="width: 100%;"
         @click="login"
-        >登录</el-button
+        >Sign in</el-button
       >
     </el-form-item>
   </el-form>
 </template>
 <script>
+import md5 from 'js-md5';
 import { requestLogin } from '@/utils';
 
 export default {
@@ -50,11 +53,10 @@ export default {
       },
       rules: {
         username: [
-          { required: true, message: '用户名不能为空！', trigger: 'blur' },
-          { type: 'number', message: '请输入学号或工号！', trigger: 'blur' },
+          { required: true, message: 'please input username', trigger: 'blur' },
         ],
         password: [
-          { required: true, message: '密码不能为空！', trigger: 'blur' },
+          { required: true, message: 'please input password', trigger: 'blur' },
         ],
       },
       remember: false,
@@ -66,16 +68,21 @@ export default {
       this.$refs.loginForm.validate((valid) => {
         if (valid) {
           this.loading = true;
-          requestLogin(this.loginForm).then((data) => {
+          const params = {
+            user: this.loginForm.username,
+            password: md5(`${this.loginForm.username}#${this.loginForm.password}`),
+          };
+          requestLogin(params).then((data) => {
             const { token, userinfo } = data;
             this.loading = false;
             this.$store.commit('setToken', token);
             this.$store.commit('setUserinfo', userinfo);
             this.$router.push({ path: '/home' });
-            this.$message.success('登录成功');
+            this.$message.success('login successfully');
           }).catch((err) => {
             this.loading = false;
             this.$message.error(err);
+            console.log(err);
           });
         }
       });
